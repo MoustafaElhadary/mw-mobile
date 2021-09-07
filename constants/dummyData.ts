7d95b381279499859f3f3783bc72035cf2ebcd71
moustafaElhadary <moustafaElhadary96@gmail.com>
2021-09-06 23:52:10 -0400

    setup

---
+++ constants/dummyData.ts
@@ -0,0 +1,113 @@
+export const myProfile = {
+    name: "ByProgrammers",
+    profile_image: require("../assets/images/profile.png"),
+    address: "No. 88, Jln Padungan, Kuching"
+}
+
+export const categories = [
+    {
+        id: 1,
+        name: "Fast Food",
+        icon: require("../assets/icons/burger.png")
+    },
+    {
+        id: 2,
+        name: "Fruit Item",
+        icon: require("../assets/icons/cherry.png")
+    },
+    {
+        id: 3,
+        name: "Rice Item",
+        icon: require("../assets/icons/rice.png")
+    }
+]
+
+export const hamburger = {
+    id: 1,
+    name: "Hamburger",
+    description: "Chicken patty hamburger",
+    categories: [1, 2],
+    price: 15.99,
+    calories: 78,
+    isFavorite: true,
+    image: require("../assets/dummyData/hamburger.png")
+}
+
+export const hotTacos = {
+    id: 2,
+    name: "Hot Tacos",
+    description: "Mexican tortilla & tacos",
+    categories: [1, 3],
+    price: 10.99,
+    calories: 78,
+    isFavorite: false,
+    image: require("../assets/dummyData/hot_tacos.png")
+}
+
+export const vegBiryani = {
+    id: 3,
+    name: "Veg Biryani",
+    description: "Indian Vegetable Biryani",
+    categories: [1, 2, 3],
+    price: 10.99,
+    calories: 78,
+    isFavorite: true,
+    image: require("../assets/dummyData/veg_biryani.png")
+}
+
+export const wrapSandwich = {
+    id: 4,
+    name: "Wrap Sandwich",
+    description: "Grilled vegetables sandwich",
+    categories: [1, 2],
+    price: 10.99,
+    calories: 78,
+    isFavorite: true,
+    image: require("../assets/dummyData/wrap_sandwich.png")
+}
+
+export const menu = [
+    {
+        id: 1,
+        name: "Featured",
+        list: [
+            hamburger, hotTacos, vegBiryani,
+        ]
+    },
+    {
+        id: 2,
+        name: "Nearby you",
+        list: [
+            hamburger, vegBiryani, wrapSandwich,
+        ]
+    },
+    {
+        id: 3,
+        name: "Popular",
+        list: [
+            hamburger, hotTacos, wrapSandwich,
+        ]
+    },
+    {
+        id: 4,
+        name: "Newest",
+        list: [
+            hamburger, hotTacos, vegBiryani,
+        ]
+    },
+    {
+        id: 5,
+        name: "Trending",
+        list: [
+            hamburger, vegBiryani, wrapSandwich,
+        ]
+    },
+    {
+        id: 6,
+        name: "Recommended",
+        list: [
+            hamburger, hotTacos, wrapSandwich,
+        ]
+    },
+
+]
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export * from "./constants";
+export * from "./theme";
+export * from "./images";
+export *  from "./icons";
+export *  from "./dummyData"
+
+
